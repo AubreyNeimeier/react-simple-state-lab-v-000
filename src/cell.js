6d00ec6c0f1475de957e7267237399877e5d02b2
AubreyNeimeier <neimeier.1@osu.edu>
2019-06-16 19:07:58 -0700

    creating the cell objects

---
+++ src/cell.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Cell extends Component{
+    constructor(props) {
+        super(props)
+        // ...define initial state with a key of 'color' set to the 'value' prop
+        this.state ={
+            color: this.props.value
+        }
+      }
+
+      update = () => {
+          this.setState({
+              color: "#333"
+          })
+
+      }
+
