c9a8c241fc4f2852c7486ec818aba0df72a96bc4
AubreyNeimeier <neimeier.1@osu.edu>
2019-06-16 19:22:41 -0700

    notes on the index file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { pattern1 } from './data.js'
-import Matrix from './Matrix.js'
-
-ReactDOM.render(<Matrix values={pattern1} />, document.getElementById('root'));
+    // index.js
+    import React from 'react';
+    import ReactDOM from 'react-dom';
+    import './index.css';
+    import { pattern1 } from './data.js'
+    import Matrix from './Matrix.js'
+    
+    ReactDOM.render(<Matrix values={pattern1} />, document.getElementById('root'));
+    // remember to import all frameworks 
